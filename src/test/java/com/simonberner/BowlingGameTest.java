6e38af3be0aab175a7a3e24a73ce5ed9e33465eb
Craig Risi <39186238+CraigRisiAG@users.noreply.github.com>
2023-05-29 12:48:42 +0200

    Update BowlingGameTest.java

--- src/test/java/com/simonberner/BowlingGameTest.java
+++ src/test/java/com/simonberner/BowlingGameTest.java
@@ -24,24 +24,15 @@ class BowlingGameTest {
     @DisplayName("Should be able to handle a spare")
     void testASpare() {
         // knock down all the pins with 2 rolls
-        rollSpare();
         // 1st roll of the next round and then finish the game
-        bowlingGame.roll(2);
         // the total point for the round should be 14
-        assertEquals(14, bowlingGame.score());
     }
 
     // 3th test
     @Test
     @DisplayName("Should be able to handle a strike")
     void testAStrike() {
-        // knock down 10 pins in the first roll of this round
-        rollStrike();
-        // knock down 8 pins in the two rolls of the next round
-        rollMany(2, 3);
-        // the total points for the round should be 18
-        assertEquals(22, bowlingGame.score());
-
+        
     }
 
 
@@ -64,9 +55,7 @@ class BowlingGameTest {
     @Test
     @DisplayName("Should be able to handle a perfect game")
     void testAPerfectGame() {
-        // 10 strikes in every of the 10 rounds plus 1 strike with the extra roll in the 10th round
-        rollMany(11, 10);
-        assertEquals(290, bowlingGame.score());
+        
 
     }
 
@@ -81,35 +70,18 @@ class BowlingGameTest {
     @Test
     @DisplayName("Should be able to handle a perfect spare game")
     void testAPerfectSpareGame() {
-        // 10 spares in every of the 10 frames plus 1 strike with the third extra roll in the 10th round
-        rollPerfectSpare();
-        // And an extra roll (strike) in the 10th frame
-        bowlingGame.roll(10);
-        assertEquals(191, bowlingGame.score());
+       
 
     }
 
     private void rollSpare() {
-        bowlingGame.roll(2);
-        bowlingGame.roll(8);
+
     }
 
     private void rollPerfectSpare() {
         for (int i = 0; i < 10; i++) {
-            bowlingGame.roll(9);
-            bowlingGame.roll(1);
+
         }
-    }
-
-    private void rollStrike() {
-        bowlingGame.roll(10);
-    }
-
-    private void rollMany(int numberOfRolls, int pins) {
-        for (int i = 0; i < numberOfRolls; i++) {
-            bowlingGame.roll(pins);
-        }
-
     }
 
 }
